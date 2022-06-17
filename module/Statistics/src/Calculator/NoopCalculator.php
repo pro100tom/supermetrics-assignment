855c6b42847ed2b4a194d0386ebcc33608e83171
Tomas Baranauskas <hubdev@hub-mdp.co.uk>
2022-06-17 14:04:27 +0200

    feature/average-number-of-posts Implementing the calculation and visualisation of the average number of posts per user per month.

--- module/Statistics/src/Calculator/NoopCalculator.php
+++ module/Statistics/src/Calculator/NoopCalculator.php
@@ -4,24 +4,37 @@ declare(strict_types = 1);
 
 namespace Statistics\Calculator;
 
+use DivisionByZeroError;
 use SocialPost\Dto\SocialPostTo;
 use Statistics\Dto\StatisticsTo;
 
 class NoopCalculator extends AbstractCalculator
 {
+    protected const UNITS = 'posts';
+
+    /**
+     * @var array
+     */
+    private array $posts = [];
+
     /**
      * @inheritDoc
      */
     protected function doAccumulate(SocialPostTo $postTo): void
     {
-        // Noops!
+        $this->posts[] = $postTo;
     }
 
     /**
      * @inheritDoc
+     * @throws DivisionByZeroError
      */
     protected function doCalculate(): StatisticsTo
     {
-        return new StatisticsTo();
+        $authorIds = array_map(function ($post) { return $post->getAuthorId(); }, $this->posts);
+        $postCountPerUser = array_count_values($authorIds);
+        $postCountPerUserAverage = array_sum($postCountPerUser) / count($postCountPerUser);
+
+        return (new StatisticsTo())->setValue(round($postCountPerUserAverage, 2));
     }
 }
---
+++ tests/unit/NoopCalculatorTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Tests\unit;
+
+use DateTime;
+use DivisionByZeroError;
+use PHPUnit\Framework\TestCase;
+use SocialPost\Dto\SocialPostTo;
+use Statistics\Calculator\NoopCalculator;
+use Statistics\Dto\ParamsTo;
+use Statistics\Enum\StatsEnum;
+
+class NoopCalculatorTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testCalculateValue(): void
+    {
+        $calculator = new NoopCalculator();
+        $params = new ParamsTo();
+        $params->setStartDate(new DateTime('-1 day'));
+        $params->setEndDate(new DateTime('+1 day'));
+        $params->setStatName(StatsEnum::AVERAGE_POST_NUMBER_PER_USER);
+        $calculator->setParameters($params);
+
+        $data = [
+            ['author_id' => 'one', 'text' => 'dummy text'],
+            ['author_id' => 'one', 'text' => 'another dummy text'],
+            ['author_id' => 'two', 'text' => 'dummy text again'],
+            ['author_id' => 'three', 'text' => 'still dummy text'],
+            ['author_id' => 'four', 'text' => 'something smart'],
+        ];
+
+        foreach ($data as $datum) {
+            $socialPostTo = new SocialPostTo();
+            $socialPostTo->setAuthorId($datum['author_id']);
+            $socialPostTo->setText($datum['text']);
+            $socialPostTo->setDate(new DateTime());
+            $calculator->accumulateData($socialPostTo);
+        }
+
+        $this->assertEquals(round(5 / 4, 2), round($calculator->calculate()->getValue(), 2));
+    }
+
+    /**
+     * @return void
+     */
+    public function testCalculateThrowsDivisionByZeroException(): void
+    {
+        $this->expectException(DivisionByZeroError::class);
+
+        $calculator = new NoopCalculator();
+        $params = new ParamsTo();
+        $params->setStartDate(new DateTime('-1 day'));
+        $params->setEndDate(new DateTime('+1 day'));
+        $params->setStatName(StatsEnum::AVERAGE_POST_NUMBER_PER_USER);
+        $calculator->setParameters($params);
+
+        $calculator->calculate();
+    }
+}
